ea2d465400c68a7aaa60b476e52112bd6b7f4788
SJahnavi20 <119915877+SJahnavi20@users.noreply.github.com>
2022-12-07 08:39:43 +0530

    Add files via upload

---
+++ QuizApp/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Quiz from './QuizApp/Quiz';
+
+
+function App() {
+    return (
+      <div className="App">
+        <h1 align="center" Style="color:#17202A;">Play The Quiz!</h1>
+        <Quiz/>
+      </div>
+    );
+  
+}
+
+export default App;
